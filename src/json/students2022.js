55147a92ee6c913ba902ea95c09dff8dc59878f8
lucygolden <106996194+lucygolden@users.noreply.github.com>
2022-07-14 14:36:33 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -11,6 +11,7 @@ const students = {
   "Alan M": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Gibson T": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Neeve R": ["https://platform-jumping.zebra06.repl.co/"],
+  "Lucy G": ["https://1919-Game.lucygolden.repl.co"],
 };
 
 const studentsSorted = Object.keys(students)
