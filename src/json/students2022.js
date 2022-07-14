77839205f419ea96bdb975be4e991de8b4c304d0
Tristan646 <107204282+Tristan646@users.noreply.github.com>
2022-07-14 14:58:39 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -11,6 +11,7 @@ const students = {
   "Alan M": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Gibson T": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Neeve R": ["https://platform-jumping.zebra06.repl.co/"],
+  "Isaac H": ["https://Tank-game2.tristan646.repl.co"]
 };
 
 const studentsSorted = Object.keys(students)
