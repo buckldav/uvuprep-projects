3c539c593243c50740efb4a4f5858e0f94a00005
Orcowles <106996368+Orcowles@users.noreply.github.com>
2022-07-14 14:32:58 -0600

    Update students2022.js

--- src/json/students2022.js
+++ src/json/students2022.js
@@ -11,6 +11,7 @@ const students = {
   "Alan M": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Gibson T": ["https://replit.com/@Jibusentanna/DinoGameP5Starter-3"],
   "Neeve R": ["https://platform-jumping.zebra06.repl.co/"],
+  "Owen C":["https://quandale-dingle-escape.orcowles.repl.co/"],v
 };
 
 const studentsSorted = Object.keys(students)
